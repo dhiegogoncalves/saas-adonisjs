0bcce10fcc5d312bf85c0ac1f030db3b8b4d5ace
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-11-05 17:09:05 -0400

    Implementação do layout da Tela SignIn

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from '../pages/Main';
 import SignUp from '../pages/Auth/SignUp';
-import SignIn from '../pages/Auth/SignUp';
+import SignIn from '../pages/Auth/SignIn';
 
 const Routes = () => (
   <BrowserRouter>
