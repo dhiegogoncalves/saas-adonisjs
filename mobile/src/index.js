b3955fd2b77363e8cbc8b18b0c5d20a2d7731bdf
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-11-08 19:55:48 -0400

    Inicio do projeto mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import './config/StatusBarConfig';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+export default App;
